c2bde06ac2344b4d8af31ad4e88225ea567bc700
Nikolas Morado <nik@mora.do>
2024-02-13 17:53:09 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,54 @@
 use x11rb::connection::Connection;
 use x11rb::protocol::randr;
+use x11rb::protocol::xinerama;
+
+const SLEEP_TIME: u64 = 1;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (conn, screen_num) = x11rb::connect(None).unwrap();
 
-    print!("Connected to server ");
+    println!();
+    println!("Connected to server ");
+
+
     let screen = &conn.setup().roots[screen_num];
+ 
+    let resources: randr::GetScreenResourcesReply =
+        randr::get_screen_resources(&conn, screen.root)?
+            .reply()?
+            .clone();
 
-    let resources: randr::GetScreenResourcesReply = randr::get_screen_resources(&conn, screen.root)?.reply()?.clone();
-    
-    let mut crtc_gammas: randr::GetCrtcGammaReply = Vec::new();
+    println!("Found {} crtcs", resources.crtcs.len());
 
-    for crtc in resources.crtcs.clone() {
-        let gamma: randr::GetCrtcGammaReply = randr::get_crtc_gamma(&conn, crtc)?.reply()?;
-        crtc_gammas.push(gamma.clone());
-        let red_black = vec![0; gamma.red.len() as usize];
-        let green_black = vec![0; gamma.green.len() as usize];
-        let blue_black = vec![0; gamma.blue.len() as usize];
-        
-        let _set_result = randr::set_crtc_gamma(&conn, crtc, &red_black, &green_black, &blue_black);
+    for crtc in &resources.crtcs {
+        println!("Found crtc {}", crtc);
+        let gamma: randr::GetCrtcGammaReply = randr::get_crtc_gamma(&conn, *crtc)?.reply()?.clone();
 
-        while let Ok(updated_gamma) = randr::get_crtc_gamma(&conn, crtc){  
+        let gamma_size = gamma.red.len();
+
+        let black_gamma = vec![0; gamma_size];
+
+        let _set_result =
+            randr::set_crtc_gamma(&conn, *crtc, &black_gamma, &black_gamma, &black_gamma);
+
+        while let Ok(updated_gamma) = randr::get_crtc_gamma(&conn, *crtc) {
             let get_reply: randr::GetCrtcGammaReply = updated_gamma.reply()?;
-            if get_reply.red == red_black && get_reply.green == green_black && get_reply.blue == blue_black {
+            if get_reply.red == black_gamma
+                && get_reply.green == black_gamma
+                && get_reply.blue == black_gamma
+            {
                 break;
             }
         }
+
+        std::thread::sleep(std::time::Duration::from_secs(SLEEP_TIME));
+
+        let _set_result = randr::set_crtc_gamma(&conn, *crtc, &gamma.red, &gamma.green, &gamma.blue);
     }
 
-
-    std::thread::sleep(std::time::Duration::from_secs(3));
-
-    for crtc in resources.crtcs.clone() {
-        let index = resources.crtcs.clone().iter().position(|&x| x == crtc).unwrap();
-        let gamma = &crtc_gammas[index];
-        let _ = randr::set_crtc_gamma(&conn, crtc, &gamma.red, &gamma.green, &gamma.blue);
-    }
+    println!();
 
     drop(conn);
 
     Ok(())
 }
- 
