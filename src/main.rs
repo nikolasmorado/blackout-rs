f019a57d69aa7e50fe3b052bf9094c008468be87
Nikolas Morado <nik@mora.do>
2024-05-18 13:25:55 -0400

    version 1.0

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,60 @@
+use clap::Parser;
 use x11rb::connection::Connection;
-use x11rb::protocol::randr::{get_crtc_gamma, get_crtc_info, get_screen_resources, set_crtc_gamma};
+use x11rb::protocol::randr::{
+    get_crtc_gamma, get_crtc_info, get_screen_resources, set_crtc_gamma, GetCrtcGammaReply,
+};
 
+#[derive(Parser)]
+struct Cli {
+    #[arg(short, long)]
+    list: bool,
+
+    #[arg(short, long)]
+    screens: Vec<u32>,
+
+    #[arg(short, long, default_value = "60")]
+    duration: u64,
+}
+
+fn blackout(conn: &impl Connection, crtc: u32) -> Result<(), Box<dyn std::error::Error>> {
+    let gamma = get_crtc_gamma(conn, crtc)?.reply()?;
+
+    for _ in 0..gamma.red.len() {
+        let mut red = gamma.red.clone();
+        let mut green = gamma.green.clone();
+        let mut blue = gamma.blue.clone();
+
+        for j in 0..red.len() {
+            red[j] = 0;
+            green[j] = 0;
+            blue[j] = 0;
+        }
+
+        set_crtc_gamma(conn, crtc, &red, &green, &blue)?;
+    }
+
+    Ok(())
+}
+
+fn restore(
+    conn: &impl Connection,
+    crtc: u32,
+    gamma: &GetCrtcGammaReply,
+) -> Result<(), Box<dyn std::error::Error>> {
+    set_crtc_gamma(conn, crtc, &gamma.red, &gamma.green, &gamma.blue)?;
+
+    Ok(())
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::parse();
+
+    if !args.list && args.screens.len() == 0 {
+        println!("No command or screens specified, try: blackout --help");
+
+        return Ok(());
+    }
+
     let (conn, screen_num) = x11rb::connect(None).unwrap();
     let screen = &conn.setup().roots[screen_num];
 
@@ -11,6 +63,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut backup_crtcs = Vec::new();
 
     for c in sr.crtcs {
+        if !args.list && !args.screens.contains(&c) {
+            continue;
+        }
+
         let crtc = get_crtc_info(&conn, c, 0)?.reply()?;
 
         if crtc.width == 0 || crtc.height == 0 {
@@ -20,35 +76,32 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("CRTC {:0?}: {:1?}x {:2?}y", c, crtc.x, crtc.y);
         println!("::: {:0?}x {:1?}y", crtc.width, crtc.height);
 
+        if args.list {
+            continue;
+        }
+
         let gamma = get_crtc_gamma(&conn, c)?.reply()?;
 
         backup_crtcs.push((c, gamma.clone()));
 
-        for _ in 0..gamma.red.len() {
-            let mut red = gamma.red.clone();
-            let mut green = gamma.green.clone();
-            let mut blue = gamma.blue.clone();
-
-            for j in 0..red.len() {
-                red[j] = 0;
-                green[j] = 0;
-                blue[j] = 0;
-            }
-
-            set_crtc_gamma(&conn, c, &red, &green, &blue)?;
-        }
+        blackout(&conn, c)?;
     }
-    
-    std::thread::sleep(std::time::Duration::from_secs(2));
+
+    if args.list {
+        return Ok(());
+    }
+
+    std::thread::sleep(std::time::Duration::from_secs(args.duration));
 
     for b in backup_crtcs {
-        println!("Setting gamma for CRTC: {:?}", b.0);
-        set_crtc_gamma(&conn, b.0, &b.1.red, &b.1.green, &b.1.blue)?;
+        if !args.screens.contains(&b.0) {
+            continue;
+        }
+
+        restore(&conn, b.0, &b.1)?;
     }
 
     let _f = conn.flush();
 
-    loop {
-        println!("Event: {:?}", conn.wait_for_event()?);
-    }
+    Ok(())
 }
